986faec740737cc4b51fb8d81ee8d6ad6101b3d2
chbarker123 <57897558+chbarker123@users.noreply.github.com>
2022-05-03 05:58:37 +0000

    project5

--- MyFourthProject/App copy.js
+++ MyFourthProject/App copy.js
@@ -1,49 +1,23 @@
 import React from 'react';
-import { Text, Image, ScrollView } from 'react-native';
+import { Text, View, FlatList } from 'react-native';
 
-const dog = {
-  url: 'https://githubusercontent.com/AbdunabiRamadan/CIS340/master/images/dog2.png',
-  width: 64,
-  height: 64
-};
+export default StatesApp = () => {
+  return (
+    <><View style={{ flex: 1, paddingTop: 22 }} /><FlatList>
+      data={[
+        { key: 'Alabama' },
+        { key: 'Alaska' },
+        { key: 'Arizona' },
+        { key: 'California' },
+        { key: 'Colorado' },
+        { key: 'Delaware' },
+        { key: 'Florida' },
+        { key: 'Georgia' },
+        { key: 'Hawaii' }
+      ]}
+      renderItem={({item}) => <Text style={{padding:10, fontSize:20, height:44}}> {item.key} </Text>}
+    </FlatList></>
 
-export default MyScrollViewApp = () => (
 
-        <ScrollView style={{padding: 40}}>
-          <Text style={{fontSize: 80}}>Try To Scroll Down!</Text>
-          <Image source={require('./assets/dog2.png')} style={{width: 64, height: 64}}/>
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Text style={{fontSize: 80}}>Try To Scroll Down More!</Text>
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-          <Image source={dog} />
-        </ScrollView>
-)
+  );
+  }
